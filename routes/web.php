2c5eb583030d0f4447e88c2a2f15c5ef2e55435f
DESKTOP-T7K32EA\user <surya.a@sproutwings.in>
2023-07-17 23:51:58 +0530

    Datatables-Server Side Works

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/', [DashboardController::class,'index'])->name('dashboard');
     Route::resource('/setting', SettingController::class);
     Route::resource('/tripplanreport', TripplanReportController::class);
+    Route::get('/trip-data',[TripplanReportController::class, "getData"]);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
